d8c6ea1ff80e8f65bd6d4bdfa298fc6700e8f3eb
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2021-08-17 16:30:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My 1st console project");
+var firstname="Siddhaerth";
+console.log(firstname);
+
+var lastname=".S";
+console.log(lastname);
+
+var full_name = first_name.concat(last_name);
+ console.log(full_name);
+ 
+ var square_root = Math.sqrt(16);
+ console.log(square_root);
+ 
+ var maximum_number = Math.max(15, 10);
+  console.log(maximum_number);
+   
+  var minimum_number = Math.min(15, 10);
+   console.log(minimum_number); 
+   
+   var power_eg_1 = Math.pow(4, 2);
+    console.log(power_eg_1);
+    
+    var power_eg_2 = Math.pow(4, 3);
+     console.log(power_eg_2);
